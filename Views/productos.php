58db3de38c193f31d363603e390a377912ce7b78
Claudio Alejandro Guerrero Nocedal <alex_nocedal@hotmail.com>
2019-10-21 19:24:27 -0500

    Resolucion disegn productos

--- Views/productos.php
+++ Views/productos.php
@@ -46,7 +46,7 @@ privilegios("Superiores");
                                 <button class="d-lg-none btn btn-danger col-6 mb-3 p-3 agrega mostra" id="Bcodigobarra"  data-toggle="modal" data-target="#modalFormCodigo">Imprimir Codigos</button>
                                 <p id="stockrequerido"></p>
                             </div>
-                            
+
                             <div class="ml-0 ml-lg-3 input-group-prepend">
                                 <div class="input-group-text"><i class="fa fa-search"></i>
                                 </div>
@@ -125,15 +125,24 @@ privilegios("Superiores");
                           </select>
                         </div>
                       </div>
-                      <div class="col-lg-4">
+                      <div class="col-lg-12">
                         <h5 class="general">Marca:</h5>
-                        <input id="marca" class="form form-control" onkeypress="return check(event)" type="text" name="Tmarca" placeholder="Marca" autocomplete="new-password" >
+                        <select class="form form-control" id="marca" name="Smarca">
+                          <option value="patito">patito</option>
+                        </select>
                       </div>
                     </div>
+                    <div class="col-lg-4">
+                      <h5 class="general">Proveedor:</h5>
+                      <input id="proveedor" class="form form-control" onkeypress="return check(event)" type="number" name="Nproveedor" placeholder="Proveedor" autocomplete="new-password" >
+                    </div>
+                  </div>
                     <div class="d-block d-lg-flex row">
-                      <div class="col-lg-4">
+                      <div class="col-lg-12">
                         <h5 class="general">Color:</h5>
-                        <input id="color" class="form form-control" type="text" onkeypress="return check(event)" name="Tcolor" placeholder="Color" autocomplete="new-password"><br>
+                        <select class="form form-control" id="color" name="Scolor">
+                          <option value="rojo">rojo</option>
+                        </select>
                       </div>
                       <div class="col-lg-4">
                         <h5 class="general">Imagen:</h5>
@@ -188,7 +197,7 @@ privilegios("Superiores");
               </div>
               <!-- Modal -->
         <script src="../js/user_jquery.js"></script>
-        <script src="../js/clientes.js"></script>
+        <script src="../js/productos.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
       </body>
--- js/productos.js
+++ js/productos.js
@@ -12,4 +12,26 @@ $.ajax({
       $("#").val(datos[1]);
   }
   });
+  $("#formulario").submit(function (e) {
+
+    $.post("../Controllers/productos.php",$("#formulario").serialize(), function (response) {
+      console.log(response);
+      $("#mensaje").css("display", "block");
+      if (response == "1") {
+        if(editar == true){
+          $('.modal').modal('hide');
+          $("#mensaje").css("display", "none");
+        }
+        $("#mensaje").text("Registro Exitoso");
+        $("#mensaje").css("color", "green");
+        $("#email").focus();
+        $("#formulario").trigger("reset");
+      } else {
+        $("#mensaje").text("Registro fallido");
+        $("#mensaje").css("color", "red");
+        $("#email").focus();
+      }
+
+      obtenerDatosTablaUsuarios();
+    });
 });
