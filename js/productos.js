0d0cee5239dca52d46b73ef581b52db9eb6425e9
Claudio Alejandro Guerrero Nocedal <alex_nocedal@hotmail.com>
2019-10-23 11:57:34 -0500

    Mostrar Productos

--- js/productos.js
+++ js/productos.js
@@ -95,11 +95,71 @@ console.log("entro al js");
       contentType: false,
       processData: false,
 
-      success: function(response) {
-          console.log("Respuesta: "+response);
-
+      success: function (response) {
+        console.log(response);
+        let datos = JSON.parse(response);
+        let template = "";
+        $.each(datos, function (i, item) {
+          template += `
+          <tr>
+                <td class="text-nowrap text-center">${item[0]}</td>
+                <td class="text-nowrap text-center">${item[1]}</td>
+                <td class="text-nowrap text-center">${item[2]}</td>
+                <td class="text-nowrap text-center">${item[3]}</td>
+                <td class="text-nowrap text-center">${item[4]}</td>
+                <td class="text-nowrap text-center">${item[5]}</td>
+                <td class="text-nowrap text-center">${item[6]}</td>
+                <td class="text-nowrap text-center">${item[7]}</td>
+                <td class="text-nowrap text-center">${item[8]}</td>
+                <td class="text-nowrap text-center">${item[9]}</td>
+                <td class="text-nowrap text-center">${item[10]}</td>
+                <td class="text-nowrap text-center">${item[11]}</td>
+                <td class="text-nowrap text-center">${item[12]}</td>
+                <td class="text-nowrap text-center">${item[13]}</td>
+                <td class="text-nowrap text-center">${item[14]}</td>
+                <td class="text-nowrap text-center d-none">${item[15]}</td>
+                <th class="text-nowrap text-center" style="width:100px;">
+                <div class="row">
+                    <a data-toggle="modal" data-target="#modalForm" style="margin: 0 auto;" class="Beditar btn btn-danger" href="#">
+                      Editar
+                    </a>
+                </div>
+                </th>
+          `;
+        });
+        $("#cuerpo").html(template);
       }
-  });
 e.preventDefault();
   });
+});
+
+$(document).on("click", ".Beditar", function () {
+  var valores = "";
+  // Obtenemos todos los valores contenidos en los <td> de la fila
+  // seleccionada
+  $(this).parents("tr").find("td").each(function () {
+    valores += $(this).html() + "?";
+  });
+  datos = valores.split("?");
+  console.log(datos);
+  $("#codigo_barras").val(datos[0]);
+  $("#modelo").val(datos[1]);
+  $("#nombre").val(datos[2]);
+  $("#descripcion").val(datos[3]);
+  $("#categoria").val(datos[4]);
+  $("#marca").val(datos[5]);
+  $("#proveedor").val(datos[6]);
+  $("#color").val(datos[7]);
+  $("#imagen").val(datos[8]);
+  $("#precio_compra").val(datos[9]);
+  $("#precio_venta").val(datos[10]);
+  $("#descuento").val(datos[11]);
+  $("#unidad_medida").val(datos[12]);
+  $("#tasa_iva").val(datos[13]);
+  $("#tasa_ipes").val(datos[14]);
+  $("#talla_numero").val(datos[15]);
+  editar = true;
+});
+
+
 });
