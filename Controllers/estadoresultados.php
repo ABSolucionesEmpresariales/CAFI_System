768665d38348e6101cb5da69a76cf2649ea16d60
DE CM <DECM24>
2019-11-02 11:11:13 -0600

    modulo estado resultados

--- Controllers/estadoresultados.php
+++ Controllers/estadoresultados.php
@@ -60,50 +60,111 @@ if (isset($_POST['combo']) && $_POST['combo'] === "negocio") {
         $datos = array("ventas" => $ventas, "costo_venta" => $costo_venta, "utilidad_bruta" =>  $ventas - $costo_venta);
         echo json_encode($datos);
     }
-} else if (isset($_POST['Ssucursal']) && isset($_POST['Dfecha1']) && isset($_POST['Dfecha2']) || isset($_POST['Ssucural']) && isset($_POST['Dmes'])) {
+} else if (isset($_POST['Ssucursal']) && isset($_POST['Dfecha1']) && isset($_POST['Dfecha2']) || isset($_POST['Ssucursal']) && isset($_POST['Dmes'])) {
     $conexion = new Models\Conexion();
-
-    if ($_POST['negocio'] != "Todos") {
-
+    if ($_POST['Ssucursal'] != "Todos") {
+        $gastos = null;
         if (!isset($_POST['Dmes'])) {
-            $datos = array($_POST['Dfecha1'], $_POST['Dfecha2'], "A", 1, $_POST['Ssucursal'], NULL, NULL, NULL, NULL, NULL);
+            $datos = array($_POST['Dfecha1'], $_POST['Dfecha2'], "A", "1", $_POST['Ssucursal'], NULL, NULL, NULL, NULL, NULL);
         } else {
             $fecha = explode("-", $_POST['Dmes']);
             $año = $fecha[0];
             $mes = $fecha[1];
-            $datos = array($mes, $año, "A", 1, $_POST['Ssucursal'], "Articulos de Venta");
+            $datos = array($mes, $año, "A", "1", $_POST['Ssucursal'], "Articulos de Venta");
             $consulta = "SELECT SUM(monto) AS total  FROM gastos WHERE MONTH(fecha) = ? AND YEAR(fecha) = ?
             AND estado = ? AND eliminado != ? AND negocio = ? GROUP BY concepto = ? ";
-            $conexion->consultaPreparada($datos,$consulta,2,"sssiss", false);
-            $datos = array($mes, $año, "A", 1, $_POST['Ssucursal'], NULL, NULL, NULL, NULL, NULL);
-            
+            $result = $conexion->consultaPreparada($datos, $consulta, 2, "ssssss", false);
+            if (isset($result) && !is_null($result[0][0])) {
+                $gastos = $result[0][0];
+            } else {
+                $gatos = 0;
+            }
+            $datos = array(NULL, NULL, NULL, NULL, NULL, $mes, $año, "A", "1", $_POST['Ssucursal']);
         }
 
-        $datos = array($_POST['Dfecha1'], $_POST['Dfecha2'], $_POST['Ssucursal']);
         $consulta = "SELECT SUM(total) AS ventas FROM venta WHERE fecha BETWEEN ? AND ?  AND estado_venta = ? AND eliminado != ? 
         AND negocio = ? OR MONTH(fecha) = ? AND YEAR(fecha) = ? AND estado_venta = ? AND eliminado != ? AND negocio = ? ";
-        $result = $conexion->consultaPreparada($datos, $consulta, 2, "sssissssss", false);
+        $result = $conexion->consultaPreparada($datos, $consulta, 2, "ssssssssss", false);
         if (isset($result) && !is_null($result[0][0])) {
             $ventas = $result[0][0];
         } else {
             $ventas = 0;
         }
 
-        
+
         $consulta = "SELECT SUM(precio_compra * stock) AS costo_venta FROM producto 
         INNER JOIN detalle_venta ON codigo_barras = detalle_venta.producto
         INNER JOIN venta ON idventa = idventas INNER JOIN stock ON codigo_barras = stock.producto
         WHERE fecha BETWEEN ? AND ?  AND estado_venta = ? AND venta.eliminado != ? AND venta.negocio = ? 
         OR MONTH(fecha) = ? AND YEAR(fecha) = ? AND estado_venta = ? AND venta.eliminado != ? AND venta.negocio = ?";
-        $result = $conexion->consultaPreparada($datos, $consulta, 2,  "sssissssss", false);
+        $result = $conexion->consultaPreparada($datos, $consulta, 2,  "ssssssssss", false);
         if (isset($result) && !is_null($result[0][0])) {
             $costo_venta = $result[0][0];
         } else {
             $costo_venta = 0;
         }
-        $datos = array("ventas" => $ventas, "costo_venta" => $costo_venta, "utilidad_bruta" =>  $ventas - $costo_venta);
+
+        if (!isset($_POST['Dmes'])) {
+            $datos = array("ventas" => $ventas, "costo_venta" => $costo_venta, "utilidad_bruta" => $utilidad_bruta = $ventas - $costo_venta);
+        } else {
+            $datos = array("ventas" => $ventas, "costo_venta" => $costo_venta, "utilidad_bruta" => $utilidad_bruta = $ventas - $costo_venta, "utilidad_neta" => $utilidad_bruta - $gastos);
+        }
+
         echo json_encode($datos);
+    } else {
+        $gastos = null;
+        if (!isset($_POST['Dmes'])) {
+            $datos = array($_POST['Dfecha1'], $_POST['Dfecha2'], "A", "1", $_SESSION['email'], NULL, NULL, NULL, NULL, NULL);
+        } else {
+            $fecha = explode("-", $_POST['Dmes']);
+            $año = $fecha[0];
+            $mes = $fecha[1];
+            $datos = array($mes, $año, "A", "1", $_SESSION['email'], "Articulos de Venta");
+            $consulta = "SELECT SUM(monto) AS total  FROM gastos
+            INNER JOIN negocios ON idnegocios = gastos.negocio 
+            INNER JOIN usuarioscafi ON email = negocios.dueno
+            WHERE MONTH(fecha) = ? AND YEAR(fecha) = ? AND gastos.estado = ? AND gastos.eliminado != ? AND dueno = ? GROUP BY concepto = ? ";
+            $result = $conexion->consultaPreparada($datos, $consulta, 2, "ssssss", false);
+            if (isset($result) && !is_null($result[0][0])) {
+                $gastos = $result[0][0];
+            } else {
+                $gatos = 0;
+            }
+            $datos = array(NULL, NULL, NULL, NULL, NULL, $mes, $año, "A", "1", $_SESSION['email']);
+        }
+        $consulta = "SELECT SUM(total) AS ventas FROM venta 
+        INNER JOIN negocios ON idnegocios = negocio
+        INNER JOIN usuarioscafi ON email = dueno
+        WHERE fecha BETWEEN ? AND ?  AND estado_venta = ? AND venta.eliminado != ? 
+        AND dueno = ? OR MONTH(fecha) = ? AND YEAR(fecha) = ? AND estado_venta = ? AND venta.eliminado != ? AND dueno = ?";
+        $result = $conexion->consultaPreparada($datos, $consulta, 2, "ssssssssss", false);
+        if (isset($result) && !is_null($result[0][0])) {
+            $ventas = $result[0][0];
+        } else {
+            $ventas = 0;
+        }
 
 
-    } else { }
+        $consulta = "SELECT SUM(precio_compra * stock) AS costo_venta FROM producto 
+        INNER JOIN detalle_venta ON codigo_barras = detalle_venta.producto
+        INNER JOIN venta ON idventa = idventas INNER JOIN stock ON codigo_barras = stock.producto
+        INNER JOIN negocios ON idnegocios = venta.negocio
+        INNER JOIN usuarioscafi ON email = negocios.dueno
+        WHERE fecha BETWEEN ? AND ?  AND estado_venta = ? AND venta.eliminado != ? AND negocios.dueno = ? 
+        OR MONTH(fecha) = ? AND YEAR(fecha) = ? AND estado_venta = ? AND venta.eliminado != ? AND negocios.dueno = ? ";
+        $result = $conexion->consultaPreparada($datos, $consulta, 2,  "ssssssssss", false);
+        if (isset($result) && !is_null($result[0][0])) {
+            $costo_venta = $result[0][0];
+        } else {
+            $costo_venta = 0;
+        }
+
+        if (!isset($_POST['Dmes'])) {
+            $datos = array("ventas" => $ventas, "costo_venta" => $costo_venta, "utilidad_bruta" => $utilidad_bruta = $ventas - $costo_venta);
+        } else {
+            $datos = array("ventas" => $ventas, "costo_venta" => $costo_venta, "utilidad_bruta" => $utilidad_bruta = $ventas - $costo_venta, "utilidad_neta" => $utilidad_bruta - $gastos);
+        }
+
+        echo json_encode($datos);
+    }
 }
