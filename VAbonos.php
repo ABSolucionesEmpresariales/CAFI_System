19628f8ee4b9b298180aa3744285abf9c941455e
Julian <julianlefunk@gmail.com>
2019-11-04 13:51:08 -0600

    l

--- VAbonos.php
+++ VAbonos.php
@@ -21,16 +21,10 @@ if (!isset($_SESSION['acceso'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/sweetalert.css">
     <link rel="icon" href="img/logo/nav1.png">
-    
-    <script src="js/sweetalert.js"></script>
-    <script src="js/sweetalert.min.js"></script>
-    <script src="js/jquery.js"></script>
-    <script src="js/index.js"></script>
 
     <title>Abonos</title>
 </head>
@@ -115,10 +109,12 @@ if (!isset($_SESSION['acceso'])) {
     <!--row-->
     </div>
     <!--container-->
-    <script src="js/user_jquery.js"></script>
-    <script src="js/vabonos.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/user_jquery.js"></script>
+    <script src="js/vabonos.js"></script>
+    <script src="js/index.js"></script>
 </body>
 
 </html>
