a0470e2fbab85a9ff9f9be08064e6ae27143fa96
Julian <julianlefunk@gmail.com>
2019-12-07 17:02:00 -0600

    Integracion landing

--- Controllers/negocio.php
+++ Controllers/negocio.php
@@ -2,9 +2,9 @@
 session_start();
 include_once '../Models/Conexion.php';
 if (
-    !empty($_POST['Tnombre']) && !empty($_POST['Sgiro']) && isset($_POST['Tcp']) && !empty($_POST['Tcalle_numero']) && !isset($_POST['Tcp']) && !empty($_POST['Tcolonia'])
+    isset($_POST['idnegocios']) && !empty($_POST['Tnombre']) && !empty($_POST['Sgiro']) && isset($_POST['Tcp']) && !empty($_POST['Tcalle_numero']) && !isset($_POST['Tcp']) && !empty($_POST['Tcolonia'])
     && !empty($_POST['DLlocalidad']) && !empty($_POST['Tmunicipio']) && !empty($_POST['Sestado'])
-    && isset($_POST['Ttelefono']) && !empty($_POST['Simpresora']) && !empty($_POST['Sdueno']) && isset($_POST['idnegocios'])
+    && isset($_POST['Ttelefono']) && !empty($_POST['Simpresora']) && !empty($_POST['Sdueno'])
 ) {
     $conexion = new Models\Conexion();
     $datos_negocio = array(
@@ -13,7 +13,6 @@ if (
         $_POST['Sgiro'],
         $_POST['Tcp'],
         $_POST['Tcalle_numero'],
-        $_POST['Tcp'],
         $_POST['Tcolonia'],
         $_POST['DLlocalidad'],
         $_POST['Tmunicipio'],
@@ -23,10 +22,13 @@ if (
         $_POST['Simpresora'],
         $_POST['Sdueno']
     );
- $consulta = "INSERT INTO negocios (idnegocios,nombre,giro,cp,calle_numero,colonia,localidad,municipio,estado,pais,telefono,impresora,dueno) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $consulta = "INSERT INTO negocios (idnegocios,nombre,giro,cp,calle_numero,colonia,localidad,municipio,estado,pais,telefono,impresora,dueno) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
         $tipo_datos = "sssssssssssss";
-        echo $conexion->consultaPreparada($datos_negocio, $consulta, 1, $tipo_datos, false,null);
-        //respuesta al front
+        if($conexion->consultaPreparada($datos_negocio, $consulta, 1, $tipo_datos, false,null)){
+            echo $conexion->optenerId();//Si se hace la consulta que retorne al javascript el ultimo id insertado en negocios, para poder insertarlo en suscripcion.
+        }else{
+            echo "0";
+        }
     }else if (isset($_POST['tabla']) && $_POST['tabla'] === "tabla") {
     //obtencion del json para pintar los renglones de la tabla
     $conexion = new Models\Conexion();
